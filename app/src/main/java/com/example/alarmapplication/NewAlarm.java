a31375c2ffecda79577340c6763ca31bca3d858a
Steve <w0263155@nscc.ca>
2019-03-23 23:32:28 -0300

    Added back button

--- app/src/main/java/com/example/alarmapplication/NewAlarm.java
+++ app/src/main/java/com/example/alarmapplication/NewAlarm.java
@@ -10,7 +10,6 @@
  *  -A date picker that also allows for selection of weekdays for alarm repetition
  *  -A scrolling time picker
  *  -A button that fires an intent to an alarm tone selection view,
- *  -A volume bar
  *  -A button that fires an intent to a snooze preference selection view
  *  -A button to save the new alarm with selected settings
  *  -A button to cancel creation
@@ -22,6 +21,8 @@ package com.example.alarmapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -31,6 +32,10 @@ public class NewAlarm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_alarm);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.new_alarm_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
     /** Called when user taps the date button */
@@ -54,6 +59,12 @@ public class NewAlarm extends AppCompatActivity {
     /** Called when user taps the Snooze button */
     public void openNewAlarmSnoozePicker(View view) {
         Intent intent = new Intent(this, NewAlarmSnooze.class);
+        startActivity(intent);
+    }
+
+    /** Called when user taps the Cancel button*/
+    public void cancelNewAlarm(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
